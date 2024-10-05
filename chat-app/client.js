643f9fd4b770bf449c052319e79141d658751080
HabibovU <habibovulugbek22@gmail.com>
2024-10-05 21:09:57 +0500

    fixing UI

--- chat-app/client.js
+++ chat-app/client.js
@@ -1,4 +1,6 @@
+const { rejects } = require("assert");
 const net = require("net");
+const { resolve } = require("path");
 const readline = require("readline/promises");
 
 const rl = readline.createInterface({
@@ -6,25 +8,44 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+
 const client = net.createConnection(3000, "127.0.0.1", async () => {
   console.log("Connected to server");
-  const username = await rl.question("Enter your username> ");
 
-  client.write(username);
-});
+  const ask = async () => {
+    const message = await rl.question("Enter a message> ");
+    await moveCursor(0, -1);
+    await clearLine(0);
+    client.write(message);
+  };
 
-client.on("data", async (data) => {
-  console.log(data.toString() + "\n");
+  ask();
 
-  //   await writeMessage();
+  client.on("data", async (data) => {
+    console.log(); // helps to be at next line
+    await moveCursor(0, -1);
+    await clearLine(0);
+    console.log(data.toString());
+
+    ask();
+  });
 });
 
 client.on("end", () => {
   console.log("Disconnected from server");
 });
-
-async function writeMessage() {
-  const message = await rl.question("Enter a message> ");
-  client.write(message);
-  await writeMessage();
-}
